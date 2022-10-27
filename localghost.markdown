faf1decb7513a0a8bbb01263a230c7380a6d50bd
joshyhud <joshhudsondev@gmail.com>
2022-10-27 17:07:34 +0100

    localghost

---
+++ localghost.markdown
@@ -0,0 +1,7 @@
+LocalGhost
+----------
+
+
+A [Pen](https://codepen.io/Joshyhud/pen/GRGgLga) by [Josh Hudson ](https://codepen.io/Joshyhud) on [CodePen](https://codepen.io).
+
+[License](https://codepen.io/license/pen/GRGgLga).
---
+++ script.js
@@ -0,0 +1,52 @@
+var ghost = document.querySelector("#ghost");
+var container = document.querySelector(".container");
+var heading = document.querySelector(".heading");
+var ghostbody = document.querySelector("#body");
+var ghosteyeL = document.querySelector("#eyeL");
+var ghosteyeR = document.querySelector("#eyeR");
+
+ghost.onclick = function() {
+  container.classList.toggle("dark");
+  ghostbody.classList.toggle("night");
+  ghosteyeL.classList.toggle("night");
+  ghosteyeR.classList.toggle("night");
+  heading.classList.toggle('dark-heading');
+  
+    setTimeout(function () {
+      container.classList.toggle("dark");
+      ghostbody.classList.toggle("night");
+      ghosteyeL.classList.toggle("night");
+      ghosteyeR.classList.toggle("night");
+      heading.classList.toggle('dark-heading');
+    }, 2000);
+  
+};
+
+container.addEventListener("animationend", changePosition, true);
+
+function changePosition(event) {
+      let circle = event.target;
+
+      circle.style.animationName = "none";
+      
+      requestAnimationFrame(() => {
+        circle.style.animationName = "";
+      });
+
+      let circleStyle = getComputedStyle(circle);
+      let finalX = circleStyle.getPropertyValue("--xB");
+      let finalY = circleStyle.getPropertyValue("--yB");
+
+      circle.style.setProperty("--xA", finalX);
+      circle.style.setProperty("--yA", finalY);
+
+      circle.style.setProperty("--xB", getRandomNumber(0 , 1000) + "px");
+      circle.style.setProperty("--yB", getRandomNumber(100, 600) + "px");
+
+      console.log(circle);
+    }
+
+    function getRandomNumber(low, high) {
+      let r = Math.floor(Math.random() * (high - low + 1)) + low;
+      return r;
+    }
